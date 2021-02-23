692ffb0435bf932d0667a4bd7a8639d48f748a69
MiCurran <mcurran1722@gmail.com>
2021-02-22 22:22:58 -0700

    prettying up formatting

--- src/newComponents/stats/legends.js
+++ src/newComponents/stats/legends.js
@@ -1,68 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import Legend from './legend.component'
-import Button from 'react-bootstrap/Button'
-import './legends.css'
-function Legends(props){
-  const [displayLegend, setDisplayLegend] = useState(false)
-  const [legendData, setLegendData] = useState({})
-    
-   const setDisplay = ()=>{//sets the visibility of the legend component
-     setDisplayLegend(!displayLegend)
-       }
+import React, { useState, useEffect } from "react";
+import Legend from "./legend.component";
+import Button from "react-bootstrap/Button";
+import "./legends.css";
+function Legends(props) {
+  const [displayLegend, setDisplayLegend] = useState(false);
+  const [legendData, setLegendData] = useState({});
 
-  if(!props.legends){//when loading we display this
-    return(
-        <div>
-      <p>no legends</p>
-        </div>
-    )}
-    //i think what we want to do here is render an avatar element for each legend below
-    //then run an on click function that renders the full legend component when the headshot is clicked
-    //after that works, id like to put it in a slideshow bar that scrolls automatically 
-    //can do on hover show a tool tip with info and say click ffor more
-    else{
-    
-        if(displayLegend === false){//if we havent chosen to display a specific legend yet we do render an image of each legend
-            const legends = props.legends.slice(0,5)
-            //in the future I would like to change this to a dropdown or carousel
-        return(<div className="legend-container">    
+  const setDisplay = () => {
+    //sets the visibility of the legend component
+    setDisplayLegend(!displayLegend);
+  };
 
-<div className="topLegends justifyContent-center text-center my-2">
-    <h1 className="text-white">Legend Stats</h1>
-    <div className="dropdown">
-  <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Choose a Legend
-  </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-  {props.legends.map(legend=>{
-                return(<div >
-                    <a className="dropdown-item" onClick={()=>{setLegendData(legend);setDisplay()}}>{legend.legend_name_key}</a>
-                {/* <Legend legend={legend}/> */}
-                </div>)
-            })}
-  </div>
-</div>
-    <div className="d-flex flex-row my-5">
-        {legends.map(legend=>{
-            return(<div >
-                <img id="legendimg" style={{cursor:'pointer'}} onClick={()=>{setLegendData(legend);setDisplay()}} src={`${process.env.PUBLIC_URL}/${legend.legend_name_key}.png`} alt={`${legend.legend_name_key}.png`}></img>
-                </div>)
-            })
-         } 
-         </div>
-         </div>         
+  if (!props.legends) {
+    //when loading we display this
+    return (
+      <div>
+        <p>no legends</p>
       </div>
-        )}
-        else{
-            return(<div className="legends">
-                <Legend legend={legendData}/>
-                <div>
-                <Button id="escapebtn" variant="primary" onClick={setDisplay}>X</Button>{' '}
-                </div>
+    );
+  }
+  //i think what we want to do here is render an avatar element for each legend below
+  //then run an on click function that renders the full legend component when the headshot is clicked
+  //after that works, id like to put it in a slideshow bar that scrolls automatically
+  //can do on hover show a tool tip with info and say click ffor more
+  else {
+    if (displayLegend === false) {
+      //if we havent chosen to display a specific legend yet we do render an image of each legend
+      const legends = props.legends.slice(0, 5);
+      //in the future I would like to change this to a dropdown or carousel
+      return (
+        <div className="legend-container">
+          <div className="topLegends justifyContent-center text-center my-2">
+            <h1 className="text-white">Legend Stats</h1>
+            <div className="dropdown">
+              <button
+                className="btn btn-primary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Choose a Legend
+              </button>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                {props.legends.map((legend) => {
+                  return (
+                    <div>
+                      <a
+                        className="dropdown-item"
+                        onClick={() => {
+                          setLegendData(legend);
+                          setDisplay();
+                        }}
+                      >
+                        {legend.legend_name_key}
+                      </a>
+                      {/* <Legend legend={legend}/> */}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-               
-            )
-        }
+            <div className="d-flex flex-row my-5">
+              {legends.map((legend) => {
+                return (
+                  <div>
+                    <img
+                      id="legendimg"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => {
+                        setLegendData(legend);
+                        setDisplay();
+                      }}
+                      src={`${process.env.PUBLIC_URL}/${legend.legend_name_key}.png`}
+                      alt={`${legend.legend_name_key}.png`}
+                    ></img>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="legends">
+          <Legend legend={legendData} />
+          <div>
+            <Button id="escapebtn" variant="primary" onClick={setDisplay}>
+              X
+            </Button>{" "}
+          </div>
+        </div>
+      );
     }
+  }
 }
-export default Legends
+export default Legends;
--- src/newComponents/stats/user.js
+++ src/newComponents/stats/user.js
@@ -1,87 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import UserCard from './userCard.component'
-import Legends from './legends'
-import Navbar from '../Navbar/navbar.component'
-import Sidebar from './userSidebar'
-import './stat.css'
-import SearchComponent from './search.component';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import UserCard from "./userCard.component";
+import Legends from "./legends";
+import Navbar from "../Navbar/navbar.component";
+import Sidebar from "./userSidebar";
+import "./stat.css";
+import SearchComponent from "./search.component";
 import { useForm } from "react-hook-form";
-import { Redirect } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 
-
-function User(props){
-const apiKey = process.env.REACT_APP_API_KEY;
-let brawlid = props.match.params.id
-const bh = require('brawlhalla-api')(apiKey);
-const [userSearch, setUserSearch] = useState(null)
-//data is set to nested objects that will hold our api data hits
-const [data, setData] = useState({
+function User(props) {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  let brawlid = props.match.params.id;
+  const bh = require("brawlhalla-api")(apiKey);
+  const [userSearch, setUserSearch] = useState(null);
+  //data is set to nested objects that will hold our api data hits
+  const [data, setData] = useState({
     userData: {},
     rankedData: {},
-})
-var i;
-const [userList, setUserList] = useState()
-const { register, handleSubmit } = useForm();
-const onSubmit = (data, e) => {
-  bh.getBhidByName(data.firstName).then(function(users){
-    for(i=0; i< users.length; i++){
-        setUserList([
+  });
+  var i;
+  const [userList, setUserList] = useState();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data, e) => {
+    bh.getBhidByName(data.firstName)
+      .then(function (users) {
+        for (i = 0; i < users.length; i++) {
+          setUserList([
             ...users,
             {
               id: users.length,
-              brawlid: users.brawlhalla_id
-            }
-          ])
-    }
-    console.log(userList)
-    }).catch(function(error){
-    
-    });
-};
-const onError = (errors, e) => console.log(errors, e);
-useEffect(()=>{
+              brawlid: users.brawlhalla_id,
+            },
+          ]);
+        }
+        console.log(userList);
+      })
+      .catch(function (error) {});
+  };
+  const onError = (errors, e) => console.log(errors, e);
+  useEffect(() => {
     //fetch data from api
-    axios.all([
-    axios.get(`https://api.brawlhalla.com/player/${brawlid}/stats?api_key=${apiKey}`),
-    axios.get(`https://api.brawlhalla.com/player/${brawlid}/ranked?api_key=${apiKey}`)
-          ])
-          .then(response => { //sets each get request to an object in an array
-            setData({userData: response[0].data, rankedData: response[1].data}) // sets the responses from calls to data objects
-                            });
-            },[brawlid])//only run useEffect if brawlid params have changed
-  //check if data has been loaded   
+    axios
+      .all([
+        axios.get(
+          `https://api.brawlhalla.com/player/${brawlid}/stats?api_key=${apiKey}`
+        ),
+        axios.get(
+          `https://api.brawlhalla.com/player/${brawlid}/ranked?api_key=${apiKey}`
+        ),
+      ])
+      .then((response) => {
+        //sets each get request to an object in an array
+        setData({ userData: response[0].data, rankedData: response[1].data }); // sets the responses from calls to data objects
+      });
+  }, [brawlid]); //only run useEffect if brawlid params have changed
+  //check if data has been loaded
 
-
-if(!userList){
-if (!data){
-    return(<h1>Loading</h1>)
-}else{  
-    //return user ranked card info and then legends
-return(<div>  
-        <Navbar />
-        <div className="sidebar">
+  if (!userList) {
+    if (!data) {
+      return <h1>Loading</h1>;
+    } else {
+      //return user ranked card info and then legends
+      return (
+        <div>
+          <Navbar />
+          <div className="sidebar">
             <h4 className="text-white label">Enter a Player Name to search</h4>
-      
-      <form onSubmit={handleSubmit(onSubmit, onError)}>
-<input placeholder="User Name" name="firstName" ref={register} />
-<button className="btn btn-primary" type="submit">Submit</button>
-</form>
-<div className="mt-5">
-</div>
+
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
+              <input 
+                placeholder="User Name" 
+                name="firstName" 
+                ref={register} />
+              <button className="btn btn-primary" type="submit">
+                Submit
+              </button>
+            </form>
+            <div className="mt-5"></div>
+          </div>
+          <UserCard userData={data.userData} rankedData={data.rankedData} />
+          <Legends legends={data.userData.legends} />
         </div>
-        <UserCard 
-        userData = {data.userData}
-        rankedData = {data.rankedData}
-        />
-        <Legends legends={data.userData.legends}/>       
-     </div> 
-    )
-} 
-}
-else{
-  return(<Redirect to={{pathname: '/stats', state: { users: userList }}} />)
-}
+      );
+    }
+  } else {
+    return <Redirect to={{ pathname: "/stats", state: { users: userList } }} />;
+  }
 }
 
 export default User;
